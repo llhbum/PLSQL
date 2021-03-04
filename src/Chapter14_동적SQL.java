f4de2335bef99bd75625a4b8afc6ed28980802a4
llhbum <llhbum@gmail.com>
2021-03-05 00:41:15 +0900

    210305

---
+++ src/Chapter14_동적SQL.java
@@ -0,0 +1,11 @@
+public class Chapter14_동적SQL {
+    /*
+
+    * 14.1 EXECUTE IMMEDIATE문을 사용하는 방법
+        [개념]
+            - 실행 중 변경 가능한 문자열 변수 또는 문자열 상수로 제공된 SQL문을 실행/ 모든 단계가 런타임에 수행/ 동적 SQL을 사용하여 런타임에 구조를 변경할 수 있는 SQL문 생성 가능
+        [구조]
+            - EXEUTE IMMEDIATE SQL문자열 [INTO 변수목록] [USING 바인드변수목록];
+
+     */
+}
