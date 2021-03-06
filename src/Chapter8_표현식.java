bc2c6584feb60f5a6d945028dc3d289f5dc14cdb
llhbum <llhbum@gmail.com>
2021-03-07 01:06:26 +0900

    210307

---
+++ src/Chapter8_표현식.java
@@ -0,0 +1,42 @@
+public class Chapter8_표현식 {
+    /*
+    Chapter8_표현식
+        [개념]
+            - 표현식을 구성하는 요소를 크게 구분해 보면 연산자와 피 연산자다. 연산자와 피연산자가 표현식을 구성하고, 그 표현식의 연산은 하나의 결과값을 만들어 내는 것이다.
+    8.4.7 CASE 표현식
+        [종류]
+            - 단순 CASE 표현식
+                [구조]
+                    CASE <선택자> WHEN
+                    < 선택자값1 >
+
+                    CASE 선택자
+                        WHEN 선택값1 THEN 결과값1
+                        WHEN 선택값2 THEN 결과값2
+                        ...
+                        WHEN 선택값n THEN 결과값n
+                        [ELSE else 결과값]
+                    END;
+                [설명]
+                    선택자가 가질 수 있는 선택값을 하나씩 검사하는 방식
+
+            - 조사 CASE 표현식
+                [구조]
+                    CASE WHEN < 조건식1 >
+
+                    CASE
+                        WHEN 조건식1 THEN 결과값1
+                        WHEN 조건식2 THEN 결과값2
+                        ...
+                        WHEN 조건식n THEN 결과값n
+                        [ELSE else 결과값]
+                    END
+                [설명]
+                    선택자를 사용하지 않으며, WHEN에 따라오는 조건으로 조건식을 사용한다.
+
+    8.5 PL/SQL에서 내장 SQL 함수의 사용
+        - DECODE 사용불가
+            -> CASE 대치
+
+     */
+}
---
+++ src/Chapter9_SQL실행.java
@@ -0,0 +1,117 @@
+public class Chapter9_SQL실행 {
+    /*
+    Chapter9_SQL실행
+
+        9.1 SELECT문의 사용
+            [예제]
+               DECLARE
+                v_cnt NUMBER;
+               BEGIN
+                Select count(*) INTO v_cnt
+                FROM emp ;
+                DBMS_OUPUT.PUT_LINE('COUNT(*) = ' || v_cnt);
+               END;
+
+        9.2 INSERT문의 사용
+            [예제]
+               DECLARE
+                    INSERT INTO emp (empno, ename, hiredate, deptno)
+                    VALUES (9000,'홍길동',SYSDATE, 30);
+                     DBMS_OUPUT.PUT_LINE('INSERT 건수 : ' || SQL%ROWCOUNT); -- 변경된 건수 출력
+                     COMMIT;
+               END;
+
+        9.3 UPDATE문의 사용
+            [예제]
+               BEGIN
+                UPDATE emp
+                    SET deptno = 40
+                    WHERE empno = 9000;
+                    DBMS_OUPUT.PUT_LINE('UPDATE 건수:' || SQL%ROWCOUNT); -- 변경된 건수 출력
+                COMMIT;
+               END;
+
+           9.4 MERGE문의 사용
+               [개념]
+                    MERGE문은 UPSERT(UPDATE + INSERT)
+
+               [예제]
+                   BEGIN
+                    MERGE INTO emp a
+                    USING DUAL
+                        ON (a.empno = 9000)
+                    WHEN MATCHED THEN -- 사번이 9000인 로우 존재 시
+                        UPDATE
+                            SET a.comm = a.comm * 1.1
+                        WHEN NOT MATCHED THEN -- 사번이 9000인 로우 미존재 시
+                            INSERT (empno, ename, job, hiredate, sal, deptno)
+                            VALUES (9000, '홍길동', 'CLERK', SYSDATE, 3000, 10);
+                        DBMS_OUPUT.PUT_LINE('MERGE 건수 : ' || SQL%ROWCOUNT) ; -- 변경된 건수 출력
+                        COMMIT;
+                   END;
+
+           9.5 DELETE문의 사용
+                [예제]
+                   BEGIN
+                        DELETE FROM emp
+                        WHERE empno = 9000;
+                        DBMS_OUPUT.PUT_LINE('INSERT 건수 : ' || SQL%ROWCOUNT); -- 변경된 건수 출력
+                        COMMIT;
+                   END;
+
+            9.6 시퀀스 사용
+            [방법]
+                : PL/SQL문에 직접 사용하는 방식, SQL문에 포함하여 사용하는 방식
+
+            [예제]
+                - PL/SQL문에 직접 사용하는 방식
+
+                REM 시퀀스 emp_seq 생성
+                CREATE SEQUENCE emp_seq;
+                   DECLARE
+                    v_seq_value NUMBER;
+                   BEGIN
+                    --SQL 없이 시퀀스를 직접 사용하는 방법
+                    v_seq_value := emp_seq.NEXTVAL;
+
+                    DBMS_OUPUT.PUT_LINE('시퀀스 값: ' || TO_CHAR(v_seq_value));
+                   END;
+
+
+                   - SQL문에 포함하여 사용하는 방식
+                    DECLARE
+                        v_seq_value NUMBER;
+                    BEGIN
+                        -- SQL을 사용하여 시퀀스 값을 얻어옴
+                        -- "v_seq_value := emp_seq.NEXTVAL"에 비해 비효율적임
+                        SELECT emp_seq.NEXTVAL INTO v_seq_value
+                        FROM dual;
+
+                        DBMS_OUPUT.PUT_LINE('시퀀스 값 : ' TO_CHAR(v_seq_value));
+                    END;
+
+                9.7 DML문의 결괏값을 PL/SQL 변수로 반환하는 방법
+                    [개념]
+                        - INSERT, UPDATE, DELETE문의 RETURNING절이 바로 오라클이 이를 위해 제공하는 확장 기능이다.
+                    [구조]
+                        RETURNING 표현식1, 표현식2 ...
+                        INTO        변수1, 변수2 ...
+                    [예제]
+                        DECLARE
+                            c_hiredate DATE := DATE'2016-01-02';
+                            v_empno emp.empno%TYPE;
+                            v_ename emp.v_ename%TYPE;
+                            v_hiredate emp.v_hiredate%TYPE;
+                        BEGIN
+                            DELETE FROM emp WHERE empno = 9000; -- 테스트 데이터 삭제
+                            -- INSERT 후 삽입된 값을 반환
+                            INSERT INTO emp (empno, ename, hiredate, deptno)
+                                VALUES(9000, '홍길동' , c_hiredate, 40);
+                            RETURNING empno, ename, hiredate
+                                INTO v_empno, v_ename, v_hiredate;
+                            DBMS_OUPUT.PUT_LINE('사원 추가 = (' || v_empno || ', ' || v_ename || ', ' || TO_CAHR(v_hiredate, 'YYYY-MM-DD') || ')');
+
+                            COMMIT;
+                        END
+     */
+}
